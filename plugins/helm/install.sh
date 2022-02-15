231bd66900ba6ad10d05db169e3cb8ee14898a7d
缘生 <i@ysicing.me>
2022-02-15 11:12:47 +0800

    Update install.sh

--- plugins/helm/install.sh
+++ plugins/helm/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ergo ops wget https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
+# ergo ops wget https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
+ergo ops wget https://dl.ysicing.me/helm-v3.8.0-linux-amd64.tar.gz
 tar xzvfC ~/.ergo/cache/helm-v3.8.0-linux-amd64.tar.gz /tmp
 mv /tmp/linux-amd64/helm /usr/local/bin
 rm -rf /tmp/linux-amd64
