fc7aa0f300b6f80f240e77814f7b16f4137c4042
缘生 <i@ysicing.me>
2022-02-15 11:33:20 +0800

    fix helm ergo

--- plugins/helm/install.sh
+++ plugins/helm/install.sh
@@ -3,5 +3,6 @@
 # ergo ops wget https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
 ergo ops wget https://dl.ysicing.me/helm-v3.8.0-linux-amd64.tar.gz
 tar xzvfC ~/.ergo/cache/helm-v3.8.0-linux-amd64.tar.gz /tmp
-mv /tmp/linux-amd64/helm /usr/local/bin
+cp -a /tmp/linux-amd64/helm /usr/local/bin
+cp -a /tmp/linux-amd64/helm ~/.ergo/bin/ergo-helm
 rm -rf /tmp/linux-amd64
