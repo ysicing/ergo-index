a5121a49452248c456983f10f4ed526f33f3469a
ysicing <i@ysicing.me>
2022-03-29 15:59:26 +0800

    fix(typo): fix typo
    fix typo
    
    Signed-off-by: ysicing <i@ysicing.me>

--- plugins/krew/krew.sh
+++ plugins/krew/krew.sh
@@ -6,9 +6,9 @@
   ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
   KREW="krew-${OS}_${ARCH}" &&
   ergo ops wget "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "/root/.ergo/tmp/${KREW}.tar.gz" &&
+  tar zxvf "/root/.ergo/cache/${KREW}.tar.gz" &&
   ./"${KREW}" install krew
   mv ./"${KREW}" /usr/local/bin/kubectl-krew
   cp -a /usr/local/bin/kubectl-krew ~/.ergo/bin/ergo-krew
-  rm -rf "/root/.ergo/tmp/${KREW}.tar.gz"
+  rm -rf "/root/.ergo/cache/${KREW}.tar.gz"
 )
